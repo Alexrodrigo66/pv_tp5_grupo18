59c6b584ccf9dc58c33efb65c04d14c774bfb205
Francisco370 <ignacio2162001@gmail.com>
2025-06-03 01:02:32 -0300

    se armo algo de pages

---
+++ src/pages/AcercaDe.jsx
@@ -0,0 +1,9 @@
+export default function AcercaDe() {
+  return (
+    <div>
+      <h2>Acerca de</h2>
+      <p>Aplicación desarrollada por [Tu Nombre o Equipo]</p>
+      <p>Para gestionar datos de alumnos con React y Vite.</p>
+    </div>
+  );
+}
