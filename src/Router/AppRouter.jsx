172081420758e446341a6b5f8a7fea4b4b79e9a1
Alexrodrigo666 <benitezpuca@gmail.com>
2025-06-05 18:10:36 -0300

    nuevo cambio en approuter

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -6,15 +6,15 @@ import EditarAlumno from '../pages/EditarAlumno';
 import DetalleAlumno from '../pages/DetalleAlumno';
 import AcercaDe from '../pages/AcercaDe';
 
-export const AppRouter = () => {
+export const AppRouter = ({ alumnos, setAlumnos }) => {
   return (
-     <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/alumnos" element={<ListaAlumnos alumnos={alumnos} setAlumnos={setAlumnos} />} />
-            <Route path="/nuevo" element={<NuevoAlumno setAlumnos={setAlumnos} />} />
-            <Route path="/alumnos/:id/editar" element={<EditarAlumno alumnos={alumnos} setAlumnos={setAlumnos} />} />
-            <Route path="/alumnos/:id" element={<DetalleAlumno alumnos={alumnos} />} />
-            <Route path="/acerca" element={<AcercaDe />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/alumnos" element={<ListaAlumnos alumnos={alumnos} setAlumnos={setAlumnos} />} />
+      <Route path="/nuevo" element={<NuevoAlumno setAlumnos={setAlumnos} />} />
+      <Route path="/alumnos/:id/editar" element={<EditarAlumno alumnos={alumnos} setAlumnos={setAlumnos} />} />
+      <Route path="/alumnos/:id" element={<DetalleAlumno alumnos={alumnos} />} />
+      <Route path="/acerca" element={<AcercaDe />} />
     </Routes>
   );
 };
